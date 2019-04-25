b35385c5fa3d26ed28417683e14001310b0aa6a1
Brian Kim <mkim26@babson.edu>
2019-04-25 17:13:29 -0400

    merge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,10 +2,6 @@ Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of d3fb687... css
   # Code is not reloaded between requests.
   config.cache_classes = true
 
