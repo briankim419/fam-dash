5c54c34937904d5ac55e69e217db9c467c3488ec
Brian Kim <mkim26@babson.edu>
2019-04-26 17:42:28 -0400

    finished email templates

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,5 +1,9 @@
 class InviteMailer < ApplicationMailer
   def new_user_invite(invite, url_link)
+    @user = invite
+    @sender_first_name = User.find_by_id(@user.sender_id).first_name
+    @sender_last_name = User.find_by_id(@user.sender_id).last_name
+    @family_name = Family.find_by_id(@user.family_id).family_name
     @invite_info = invite
     @token = url_link
     mail(
@@ -11,8 +15,10 @@ class InviteMailer < ApplicationMailer
   def existing_user_invite(invite)
     @user = invite
     @sender = User.find_by_id(@user.sender_id).first_name
+    @sender_first_name = User.find_by_id(@user.sender_id).first_name
+    @sender_last_name = User.find_by_id(@user.sender_id).last_name
     @family_name = Family.find_by_id(@user.family_id).family_name
-    mail(to: @user.recipient.email, subject: "#{@sender} has invited you to #{@family_name}")
+    mail(to: @user.recipient.email, subject: "#{@sender_first_name} #{@sender_last_name} has invited you to #{@family_name}")
   end
 
 end
